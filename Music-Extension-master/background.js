275b0353b363959b5d3d69986935ea64589a8f04
Gaurav Barua <87770375+Tycoon786@users.noreply.github.com>
2023-12-02 00:22:01 +0530

    Delete Music-Extension-master directory

--- Music-Extension-master/background.js
+++
@@ -1,7 +0,0 @@
-chrome.browserAction.onClicked.addListener((tab) => {
-    chrome.tabs.create({
-        'url': chrome.extension.getURL('dist/index.html#/homePage')
-    }, (tab) => {
-        // Tab opened.
-    });
-});
